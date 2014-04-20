a5944a4f28e4a8b4d839817d1a5c3a55620fd184
Ben Pringle <ben.pringle@gmail.com>
2014-04-20 16:14:38 -0400

    Check IPCAs per application

--- test.py
+++ test.py
@@ -1,4 +1,24 @@
+import collections
+
+import statistics
+
 import dataio
 
-metadata = dataio.metadata()
-print(metadata.columns)
+def main():
+    graph = dataio.graph()
+    metadata = dataio.metadata()
+
+    key = 'ipcaA'
+    series_dirty = metadata[key]
+    print('Total entries:', len(series_dirty))
+    series = series_dirty.dropna()
+    print('Nan entries:', len(series_dirty) - len(series))
+
+    index_counts = collections.Counter(series.index).values()
+    print('IPCAs per appln (min):', min(index_counts))
+    print('IPCAs per appln (max):', max(index_counts))
+    print('IPCAs per appln (mean):', statistics.mean(index_counts))
+    print('IPCAs per appln (median):', statistics.median(index_counts))
+
+if __name__ == '__main__':
+    main()
